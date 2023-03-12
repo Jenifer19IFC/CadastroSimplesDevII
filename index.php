e9f00c0f1b1de9774488cafc8c16f6dad9ce5e0a
Jenifer19IFC <jenifergoedert10@gmail.com>
2023-03-12 13:56:49 -0300

    name

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>Logoft</title>
+    <title>Cognitum</title>
   </head>
   <body>
 
      <!-- Barra de navegação -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Logoft</a>
+            <a class="navbar-brand" href="#">Cognitum</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
